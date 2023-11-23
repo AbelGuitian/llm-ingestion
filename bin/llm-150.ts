8719a36c8ff3bd96ee9848dac1f77f4822fde064
AbelAbner <a.nolascoguitian@dxc.com>
2023-11-23 09:59:49 -0600

    environments with NODE_ENV

--- bin/llm-150.ts
+++ bin/llm-150.ts
@@ -4,19 +4,30 @@ import * as cdk from 'aws-cdk-lib';
 import { LlmStack } from '../lib/llm-stack';
 
 const app = new cdk.App();
-new LlmStack(app, 'Marqo-LLM-150', {
 
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const stage = process.env.NODE_ENV;
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// require('custom-env').env(`${stage}`); // load environment variables
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  //env: { account: '698588432660', region: 'us-east-1' },
+const id = `Marqo-LLM-150-${stage}`
+if (stage === 'dev' || stage === 'development') {
+  new LlmStack(app, id, {
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
+    //env: { account: '698588432660', region: 'us-east-1' },
+
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  })
+} else if (stage === 'prod' || stage === 'production') {
+  new LlmStack(app, id, {
+    env: { account:  process.env.CDK_ACCOUNT, region: process.env.CDK_REGION },
+  })
+}
--- lambda/python/manage-s3-event-notifications.py
+++ lambda/python/manage-s3-event-notifications.py
@@ -2,6 +2,7 @@ import marqo
 import boto3
 import helpers
 from botocore.response import StreamingBody
+from typing import Dict
 
 
 def marqo_test():
@@ -27,8 +28,12 @@ def handler(event, context):
 
     marqo_test()
 
+    # for local testing
+    # session = boto3.Session(profile_name='698588432660_KBIDeveloperAccess')
+    # s3_client = session.client('s3')
+
     # trying to retrieve an object from s3
-    """ s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3')
 
     for record in event['Records']:
         if record['eventName'] != 'ObjectCreated:Put':
@@ -42,12 +47,26 @@ def handler(event, context):
         s3_object: StreamingBody = s3_client.get_object(Bucket=bucket_name, Key=object_name)['Body']
         file_content_bytes = s3_object.read()
 
-        helpers.print_content(file_content_bytes)  """
+        helpers.print_content(file_content_bytes)
 
     return {
         'statusCode': 200,
         'body': event
     }
 
-
-# handler({}, {})
+# testing
+# test_event = {'Records': [
+#     {'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'us-east-1', 'eventTime': '2023-11-22T04:17:56.332Z',
+#      'eventName': 'ObjectCreated:Put', 'userIdentity': {'principalId': 'AWS:AROA2FJY5REKHBMW5QBJW:a.nolascoguitian'},
+#      'requestParameters': {'sourceIPAddress': '201.175.104.168'},
+#      'responseElements': {'x-amz-request-id': 'REEKRN0E8B84EMEQ',
+#                           'x-amz-id-2': 'ILjkRc5h0g+0TXbY2iHrMyuntVVgLLHx8z'
+#                                         '/7ClcT7ApOxptOYRJA1lfvdXtnZcuafaErOpjSfTe3endf8rieLRYSSzAFW+Vi'},
+#      's3': {'s3SchemaVersion': '1.0', 'configurationId': 'NTQ1YzRiOGUtMWIxZi00MjYwLWJhNTMtYTQ0MGEyMDcwNTg2',
+#             'bucket': {'name': 'marqo-llm-150-bucket83908e77-ojofkpqesy6y',
+#                        'ownerIdentity': {'principalId': 'ASGR0QQBM5VR5'},
+#                        'arn': 'arn:aws:s3:::marqo-llm-150-bucket83908e77-ojofkpqesy6y'},
+#             'object': {'key': 'DESCANSOS+DE+JUGADORES+1.pdf', 'size': 42150, 'eTag': '72426190cb03076b14883e80430d3dc9',
+#                        'sequencer': '00655D80F445423794'}}}]}
+#
+# handler(test_event, {})
